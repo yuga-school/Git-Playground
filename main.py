d089160bacd5a19faf72d3b6fc258bc9a7e90d78
forest <133188597+Forest815@users.noreply.github.com>
2023-10-16 10:25:53 +0900

    ボブが機能Bをリファクタリング

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import math
 # [機能A]@アリス担当 #############
 
 
-# [機能B]@ボブ担当 ###############
+def func_B ():
+  t = datetime.datetime(2023,11,4,10,00,00) # 11/4 10:00
+  diff = t - datetime.datetime.now()
+  days = diff.days
+  hours= diff.seconds // 3600
+  print(f'高専祭まで、あと{days}日と{hours}時間')
 
 
 # メイン処理 #####################
