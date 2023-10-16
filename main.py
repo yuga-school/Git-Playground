45920bd1c18c9aa1b98d3ce7febaf617148cc371
higashi <higashiyuga1@gmail.com>
2023-10-16 10:22:08 +0900

    アリスが機能Aのスタブを作成

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import math
+import fractions
 
 # [機能A]@アリス担当 #############
-
+def func_A ():
+  print('func_A called.')
 
 # [機能B]@ボブ担当 ###############
 
 
-# メイン処理 #####################
+# メイン処理 ####################
 if __name__ == "__main__": 
 
   print("start.")
 
   ## 機能Aの実行
-
+  func_A()
+  
   ## 機能Bの実行
 
   print("end.")
-  
