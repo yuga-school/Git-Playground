9ef23391d555f4d741f5befa0536ef57c2bb4b31
forest <133188597+Forest815@users.noreply.github.com>
2023-10-16 10:17:52 +0900

    ボブが機能Bを実装

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ if __name__ == "__main__":
 
   ## 機能Aの実行
 
-  ## 機能Bの実行
+  def func_B ():
+  t1 = datetime.datetime.now() # 現在時刻
+  t2 = datetime.datetime(2023,11,4,10,00,00) # 11/4 10:00
+  diff = t2 - t1
+  days = diff.days
+  hours= diff.seconds // 3600
+  print('高専祭まで、あと',end='')
+  print(days,end='')
+  print('日と',end='')
+  print(hours,end='')
+  print('時間')
 
   print("end.")
   
